f98c64734a2e767434c52f451c70dd9ef52b5732
Qubimium <173802736+Qubimium@users.noreply.github.com>
2024-06-25 19:54:24 +0200

    W FIXXXX W FIXXXX :D

---
+++ src/main/java/net/idothehax/NotEnoughLuckDataGenerator.java
@@ -0,0 +1,11 @@
+package net.idothehax;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NotEnoughLuckDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
